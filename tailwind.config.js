eed31d354f462521b7e106b1f19ab5ef4f701a47
SertralineHexal <hexal@airmail.cc>
2024-09-26 13:00:17 +0200

    Fix the Tailwind config and also add ";" to the CSS for @tailwind base, utilities, components, variants and etc so it has correct syntax, and output is successfully compiled.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Scan HTML and JS files in the src directory
+    "./*.html" // Scan HTML files in the root directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
